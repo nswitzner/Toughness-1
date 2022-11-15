1c62d38871ebf797ee099a8d2d16e53063e768f2
pipeintegrity@outlook.com <pipeintegrity@outlook.com>
2022-11-15 17:18:11 -0600

    Added in section on simplified HT method

--- maxey.R
+++ maxey.R
@@ -2,17 +2,32 @@ library(tidyverse)
 library(tidymodels)
 library(ggpmisc)
 library(patchwork)
+# library(see)
+# library(performance)
+# library(modelbased)
 
 set.seed(777)
+theme_set(theme_minimal(14))
 
 maxey <- read_csv("maxey_data.csv",
                   skip = 5,
                   col_names = TRUE) %>%
   janitor::clean_names()
 
+lm_mod <- lm(y ~ x, maxey)
+
+# predicted <- modelbased::estimate_prediction(lm_mod)
+
+# plot(predicted)
+
+
+maxtidy <- tidy(lm_mod, conf.int = TRUE) %>%
+  select(term, estimate, conf.low, conf.high) %>%
+  mutate(across(.cols = estimate:conf.high, .fns =  ~ round(.x, 3)))
+
 maxey %>%
   ggplot(aes(x, y)) +
-  geom_point() +
+  geom_point(col='orangered') +
   theme_minimal(14) +
   geom_smooth(method = "lm", se = TRUE) +
   annotate(
@@ -30,16 +45,32 @@ maxey %>%
 
   
 
-lm_mod <- lm(y ~ x, maxey)
-
 glance(lm_mod)
+
 maxey_pred_int <- augment(lm_mod,newdata = maxey,interval = "prediction")
+
 write_csv(maxey_pred_int, "maxey_pred_int.csv")
 
+maxey_pred_int %>% 
+  ggplot(aes(x = .resid))+
+  geom_histogram(aes(y =after_stat(density)),
+                 fill='royalblue2', 
+                 col='black', 
+                 bins=20) +
+  stat_function(fun=dnorm, args = list(mean(maxey_pred_int$.resid), 
+                                       sd(maxey_pred_int$.resid)),
+                col='red',
+                lwd = 1.1,
+                n =300)+
+  labs(title = "Histogram of Residuals for Maxey Data",
+       x="Residuals (Predicted - Actual)")
 
-maxtidy <- tidy(lm_mod,conf.int = TRUE) %>% 
-  select(term, estimate, conf.low, conf.high) %>% 
-  mutate(across(.cols = estimate:conf.high,.fns =  ~round(.x, 3)))
+
+maxey_pred_int %>% 
+  ggplot(aes(sample = .resid))+
+  geom_qq()+
+  geom_qq_line(col='blue')
+
 
 boots <- bootstraps(maxey,
                     times = 2e3,
@@ -102,10 +133,17 @@ histo/regress
 MASS::rlm(y ~ x, maxey)
 
 boot_aug %>% 
-  ggplot(aes(.resid))+
-  geom_histogram(fill='royalblue2', 
+  ggplot(aes(x =.resid))+
+  geom_histogram(aes(y = after_stat(density)),
+                 fill='royalblue2', 
                  col='black',
                  bins=50)+
+  stat_function(fun=dnorm, 
+                args = list(mean(boot_aug$.resid), 
+                            sd(boot_aug$.resid)), 
+                col='red', 
+                lwd =1.1, 
+                n=200)+
   theme_minimal()
 
 performance::check_model(lm_mod)
@@ -118,4 +156,9 @@ summary(max_stan)
 
 plot(max_stan, prob=0.95)
 
-
+maxey_pred_int %>% 
+  ggplot(aes(x,y))+
+  geom_point()+
+  geom_smooth(method="lm", se=FALSE, col='orange')+
+  # geom_point(aes(x, .fitted), col='red')
+  geom_ribbon(aes(x = x, ymin = .lower, ymax=.upper), fill='blue', alpha = 0.1)
